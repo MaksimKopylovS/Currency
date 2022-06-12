006a8c2f053735723cdf6b5f8027de0e7859b185
Maksim <maksim.kopylov.x5@gmail.com>
2022-06-12 15:30:50 +0300

    comment

--- src/main/java/com/max_sk/currency/CurrencyApplication.java
+++ src/main/java/com/max_sk/currency/CurrencyApplication.java
@@ -5,6 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.PropertySource;
 
+/*Добрый день, уважаемые разрабочики Альфа Банка.
+Спасибо, Вам за увлекательное задание, с большим удовольствием выполнил его для Вас.
+Благодоря полученному IT опыту в разных направлениях, и прохождению обучения
+по спецальности JAVA разрабочик могу, быстро втянутся в вашь поект и начать выполнять
+поставленные задачи, уверен, что смогу быть максимально полезным для вашей команды.*/
+
 @SpringBootApplication
 @EnableFeignClients
 @PropertySource("file:application.conf")
